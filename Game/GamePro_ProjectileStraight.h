9ade77b3573480a065f6da07458572ed33996e41
MSI\kai12 <kai1222.mikan@gmail.com>
2023-09-10 22:02:10 +0900

    色々調整板

--- Game/GamePro_ProjectileStraight.h
+++ Game/GamePro_ProjectileStraight.h
@@ -1,21 +1,46 @@
 #pragma once
 
 #include "../Lemur/Graphics/skinned_mesh.h"
-#include "GamePro_Projectile.h"
 
-class GamePro_ProjectileStraight : public GamePro_Projectile
+#include "../Lemur/Component/GameObject.h"
+#include "../Lemur/Graphics/skinned_mesh.h"
+#include "../Lemur/Graphics/Graphics.h"
+#include "../Lemur/Resource/ResourceManager.h"
+
+class GamePro_ProjectileStraight : public GameObject
 {
 public:
-    GamePro_ProjectileStraight();
-    ~GamePro_ProjectileStraight() override;
-    //çXêVèàóù
-    void Update(float elapsedTime) override;
-    //ï`âÊèàóù
-    void Render(ID3D11DeviceContext* dc, ID3D11PixelShader* replaced_pixel_shader) override;
+    GamePro_ProjectileStraight(InputComponent* input_,
+        PhysicsComponent* physics_,
+        GraphicsComponent* graphics_) :GameObject(input_, physics_, graphics_) {}
+
     //î≠éÀ
-    void Launch(const DirectX::XMFLOAT3& direction, const DirectX::XMFLOAT3& position);
+    //void Launch(GameObject* gameobj,const DirectX::XMFLOAT3& direction, const DirectX::XMFLOAT3& position);
+
+
+    float speed = 1.0f;
+};
+
+
+class GamePro_ProjectileStraightInputComponent :public InputComponent
+{
+    void Initialize(GameObject* gameobj) override {}
+    void Update(GameObject* gameobj, float elapsedTime) override;
+};
+
+class GamePro_ProjectileStraightPhysicsComponent :public PhysicsComponent
+{
+    void Initialize(GameObject* gameobj) override;
+    void Update(GameObject* gameobj, float elapsedTime) override;
+};
+
+class GamePro_ProjectileStraightGraphicsComponent :public GraphicsComponent
+{
+    void Initialize(GameObject* gameobj) override;
+    void Update(GameObject* gameobj) override;
+    void Render(GameObject* gameobj, float elapsedTime, ID3D11PixelShader* replaced_pixel_shader) override;
 
 private:
     std::shared_ptr<skinned_mesh> BulletModel = nullptr;
-    float speed = 1.0f;
+
 };
